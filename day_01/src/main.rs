d099c4a2f4697dc5cb3c205ef95833ce202b1384
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-01 17:34:20 +0100

    setup project and day_01 demo content

---
+++ day_01/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
+fn main() {
+    let input_dir: &Path = if Path::new("input").exists() {
+        Path::new("input")
+    } else {
+        Path::new("day_01/input")
+    };
+    let mut inputs: Vec<(String, PathBuf)> = Vec::new();
+    fs::read_dir(input_dir)
+        .unwrap()
+        .map(|x| x.unwrap().path())
+        .map(|x| (fs::read_to_string(&x).unwrap(), x))
+        .filter(|(file_content, name)| !file_content.is_empty())
+        .for_each(|x| inputs.push(x));
+    for (file_contents, test_name) in inputs.iter() {
+        println!("{}: {}", test_name.to_str().unwrap(), do_task(file_contents));
+    }
+}
+
+fn do_task(input: &String) -> String {
+    "42".to_string()
+}
