400553214d8dd2ce682adc2bffd800816672eb6e
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-09 09:37:15 +0100

    update template

--- day_template/src/main.rs
+++ day_template/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
 }
 
 fn do_task(input: &String) -> (i64, i64) {
-    let debug_print = std::env::var("DEBUG_PRINT").unwrap_or("0".to_string()) == "1";
+    let debug_print =
+        std::env::var("DEBUG_PRINT").unwrap_or("0".to_string()) == "1" && input.len() < 10000;
     let mut result1 = 0;
     let mut result2 = 0;
     (result1, result2)
