688b911b0313f89c986ac47137836840d2303241
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-10 09:57:23 +0100

    day_10 format

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-use tae_aoclib2025::{solve_all_inputs};
+use tae_aoclib2025::solve_all_inputs;
 
 fn main() {
     solve_all_inputs("day_10", do_task)
@@ -13,10 +13,11 @@ fn do_task(input: &String) -> (i64, i64) {
     let cols = input.lines().next().unwrap().len();
     let board = parse_input(&input);
 
-    let mut reachable_targets: Vec<Vec<HashSet<(usize, usize)>>> = vec![vec![HashSet::new(); cols]; rows];
+    let mut reachable_targets: Vec<Vec<HashSet<(usize, usize)>>> =
+        vec![vec![HashSet::new(); cols]; rows];
     let mut hiking_trails: Vec<Vec<usize>> = vec![vec![0; cols]; rows];
-    for row in 0..rows{
-        for col in 0..cols{
+    for row in 0..rows {
+        for col in 0..cols {
             if board[row][col] == 9 {
                 reachable_targets[row][col].insert((row, col));
                 hiking_trails[row][col] = 1;
@@ -24,28 +25,32 @@ fn do_task(input: &String) -> (i64, i64) {
         }
     }
     for i in (0..9).rev() {
-        for row in 0..rows{
-            for col in 0..cols{
+        for row in 0..rows {
+            for col in 0..cols {
                 if board[row][col] == i {
-                    if row + 1 < rows && board[row+1][col] == i + 1 {
-                        let reachable_from_neighbor: HashSet<(usize, usize)> = reachable_targets[row+1][col].iter().cloned().collect();
+                    if row + 1 < rows && board[row + 1][col] == i + 1 {
+                        let reachable_from_neighbor: HashSet<(usize, usize)> =
+                            reachable_targets[row + 1][col].iter().cloned().collect();
                         reachable_targets[row][col].extend(reachable_from_neighbor);
-                        hiking_trails[row][col] += hiking_trails[row+1][col];
+                        hiking_trails[row][col] += hiking_trails[row + 1][col];
                     }
-                    if col + 1 < rows && board[row][col+1] == i + 1 {
-                        let reachable_from_neighbor: HashSet<(usize, usize)> = reachable_targets[row][col+1].iter().cloned().collect();
+                    if col + 1 < rows && board[row][col + 1] == i + 1 {
+                        let reachable_from_neighbor: HashSet<(usize, usize)> =
+                            reachable_targets[row][col + 1].iter().cloned().collect();
                         reachable_targets[row][col].extend(reachable_from_neighbor);
-                        hiking_trails[row][col] += hiking_trails[row][col+1];
+                        hiking_trails[row][col] += hiking_trails[row][col + 1];
                     }
-                    if row >= 1 && board[row-1][col] == i + 1 {
-                        let reachable_from_neighbor: HashSet<(usize, usize)> = reachable_targets[row-1][col].iter().cloned().collect();
+                    if row >= 1 && board[row - 1][col] == i + 1 {
+                        let reachable_from_neighbor: HashSet<(usize, usize)> =
+                            reachable_targets[row - 1][col].iter().cloned().collect();
                         reachable_targets[row][col].extend(reachable_from_neighbor);
-                        hiking_trails[row][col] += hiking_trails[row-1][col];
+                        hiking_trails[row][col] += hiking_trails[row - 1][col];
                     }
-                    if col >= 1 && board[row][col-1] == i + 1 {
-                        let reachable_from_neighbor: HashSet<(usize, usize)> = reachable_targets[row][col-1].iter().cloned().collect();
+                    if col >= 1 && board[row][col - 1] == i + 1 {
+                        let reachable_from_neighbor: HashSet<(usize, usize)> =
+                            reachable_targets[row][col - 1].iter().cloned().collect();
                         reachable_targets[row][col].extend(reachable_from_neighbor);
-                        hiking_trails[row][col] += hiking_trails[row][col-1];
+                        hiking_trails[row][col] += hiking_trails[row][col - 1];
                     }
                 }
             }
@@ -53,15 +58,37 @@ fn do_task(input: &String) -> (i64, i64) {
     }
 
     if debug_print {
-        println!("{:}", board.iter().map(|x| x.iter().map(|c| format!("{c}")).collect::<Vec<String>>().join("")).collect::<Vec<String>>().join("\n"));
+        println!(
+            "{:}",
+            board
+                .iter()
+                .map(|x| x
+                    .iter()
+                    .map(|c| format!("{c}"))
+                    .collect::<Vec<String>>()
+                    .join(""))
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
         println!();
-        println!("{:}", reachable_targets.iter().map(|x| x.iter().map(|c| format!("{}", c.len())).collect::<Vec<String>>().join("")).collect::<Vec<String>>().join("\n"));
+        println!(
+            "{:}",
+            reachable_targets
+                .iter()
+                .map(|x| x
+                    .iter()
+                    .map(|c| format!("{}", c.len()))
+                    .collect::<Vec<String>>()
+                    .join(""))
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
     }
 
     let mut result1 = 0;
     let mut result2 = 0;
-    for row in 0..rows{
-        for col in 0..cols{
+    for row in 0..rows {
+        for col in 0..cols {
             if board[row][col] == 0 {
                 result1 += reachable_targets[row][col].len();
                 result2 += hiking_trails[row][col];
