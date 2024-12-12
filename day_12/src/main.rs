b171527c21cb36c9f4f1c217c41dd4aad5445e2e
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-12 06:51:13 +0100

    prepare day_12

---
+++ day_12/src/main.rs
@@ -0,0 +1,13 @@
+use tae_aoclib2025::{solve_all_inputs};
+
+fn main() {
+    solve_all_inputs("day_12", do_task)
+}
+
+fn do_task(input: &String) -> (i64, i64) {
+    let debug_print =
+        std::env::var("DEBUG_PRINT").unwrap_or("0".to_string()) == "1" && input.len() < 10000;
+    let mut result1 = 0;
+    let mut result2 = 0;
+    (result1, result2)
+}
