d9dc8b216d55e51cb64dd110ff0bbe29f7d912f4
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-18 08:08:02 +0100

    template update

--- day_template/src/main.rs
+++ day_template/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     solve_all_inputs("day_XX", do_task)
 }
 
-fn do_task(input: &String) -> (i64, i64) {
+fn do_task(input: &String) -> (String, String) {
     let debug_print =
         std::env::var("DEBUG_PRINT").unwrap_or("0".to_string()) == "1" && input.len() < 10000;
     let mut result1 = 0;
     let mut result2 = 0;
-    (result1, result2)
+    (format!("{}", result1), format!("{}", result2))
 }
